9af93ffca47676a0040cf79f5ba1a9e614e0ba88
Krystian <28630895+Gryfin123@users.noreply.github.com>
2023-12-27 20:44:59 +0100

    Game works fine. It still requires unit testing

---
+++ BlazorTicTacToe/Components/BoardCell.cs
@@ -0,0 +1,104 @@
+﻿using static BlazroTicTacToe.Components.Pages.Tictactoe;
+
+namespace BlazroTicTacToe.Components
+{
+    public class BoardCell
+    {
+        private CellState state = CellState.DEFAULT;
+        private bool disabled = false;
+
+        public CellState State
+        {
+            get { return state; }
+            private set { state = value; }
+        }
+        public string Text
+        {
+            get
+            {
+                switch (state)
+                {
+                    case CellState.DEFAULT:
+                        return "-";
+                    case CellState.CIRCLE:
+                        return "o";
+                    case CellState.CROSS:
+                        return "x";
+                    default:
+                        return "-";
+                }
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                switch (state)
+                {
+                    case CellState.DEFAULT:
+                        return " btn-secondary ";
+                    case CellState.CIRCLE:
+                        return " btn-success ";
+                    case CellState.CROSS:
+                        return " btn-danger ";
+                    default:
+                        return "btn-secondary";
+                }
+            }
+        }
+        public bool Disabled
+        {
+            get { return disabled; }
+        }
+        public string DisabledText
+        {
+            get
+            {
+                if (disabled)
+                {
+                    return " disabled ";
+                }
+
+                switch (state)
+                {
+                    case CellState.DEFAULT:
+                        return "";
+                    case CellState.CIRCLE:
+                        return " disabled ";
+                    case CellState.CROSS:
+                        return " disabled ";
+                    default:
+                        return "";
+                }
+            }
+        }
+
+        public BoardCell() {; }
+        public BoardCell(CellState cellState)
+        {
+            ChangeState(cellState);
+        }
+
+        // Changed button adequate to the state
+        public void ChangeState(CellState cellState)
+        {
+            state = cellState;
+        }
+        public void Disable()
+        {
+            disabled = true;
+        }
+        public void Enable()
+        {
+            disabled = false;
+        }
+
+    }
+
+    public enum CellState
+    {
+        DEFAULT,
+        CIRCLE,
+        CROSS
+    }
+}
---
+++ BlazorTicTacToe/Components/GameLogic.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.JSInterop;
+using static BlazroTicTacToe.Components.Pages.Tictactoe;
+
+namespace BlazroTicTacToe.Components
+{
+    public class GameLogic
+    {
+        // --- Events --- //
+        public event MessageDelegate? RaiseMessage;
+        public delegate void MessageDelegate(string message);
+        
+
+
+        // --- Data --- //
+        CellState currentPlayer = CellState.CROSS;
+
+
+        // Check if board is full
+        public bool IsBoardFull
+        {
+            get
+            {
+                foreach (BoardCell item in currentBoardData)
+                {
+                    if (item.State == CellState.DEFAULT)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        // Array to hold board data
+        public BoardCell[,] currentBoardData =
+        {
+        { new BoardCell(CellState.CROSS),
+            new BoardCell(),
+            new BoardCell(CellState.CROSS)},
+        { new BoardCell(),
+            new BoardCell(),
+            new BoardCell()},
+        { new BoardCell(),
+            new BoardCell(CellState.CROSS),
+            new BoardCell(CellState.CROSS)}
+    };
+
+
+        // --- Functions --- //
+        //Change player after an action
+        public void ChangePlayer()
+        {
+            if (currentPlayer == CellState.CROSS)
+            {
+                currentPlayer = CellState.CIRCLE;
+            }
+            else
+            {
+                currentPlayer = CellState.CROSS;
+            }
+        }
+
+        // Triggered when clicked on a cell
+        public void BoardCellOnClick(BoardCell cell)
+        {
+            cell.ChangeState(currentPlayer);
+
+            if (CheckIfCurrentPlayerWon())
+            {
+                LockBoard();
+
+                // Alert that the game is over and who has won
+                string playerName = "";
+                if (currentPlayer == CellState.CIRCLE)
+                {
+                    playerName = "Circle";
+                }
+                else
+                {
+                    playerName = "Cross";
+                }
+
+                RaiseMessage?.Invoke("Player with " + playerName + " won! Click Restart to try again.");
+
+                return;
+            }
+
+            if (IsBoardFull)
+            {
+                // Alert that the game is over because of no space on the board
+                RaiseMessage?.Invoke("Board is full, so the match is a draw. Click Restart to continue.");
+                return;
+            }
+
+            ChangePlayer();
+        }
+        // Check if current player has winning combination
+        public bool CheckIfCurrentPlayerWon()
+        {
+            for (int col = 0; col < currentBoardData.GetLength(0); col++)
+            {
+                for (int row = 0; row < currentBoardData.GetLength(1); row++)
+                {
+                    if (currentBoardData[col, row].State == currentPlayer)
+                    {
+                        // Checkes for lines made with current cell as center
+                        if (CheckCell(col - 1, row - 1) && CheckCell(col + 1, row + 1)) return true;
+                        if (CheckCell(col - 1, row + 1) && CheckCell(col + 1, row - 1)) return true;
+                        if (CheckCell(col - 0, row - 1) && CheckCell(col + 0, row + 1)) return true;
+                        if (CheckCell(col - 1, row - 0) && CheckCell(col + 1, row - 0)) return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        // Checks if cell at chosen coordinates is of current player
+        private bool CheckCell(int col, int row)
+        {
+            // Check if coordinates are inside of index
+            if (col < currentBoardData.GetLength(0) && col >= 0 &&
+                row < currentBoardData.GetLength(1) && row >= 0)
+            {
+                // If searched cell is in an array, check if it matches current player
+                if (currentBoardData[col, row].State == currentPlayer)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // If checked place is out of bound (what is gonna happen), return that the cell is not of interest for current player
+                return false;
+            }
+        }
+
+        // Set all buttons to be unclickable
+        public void LockBoard()
+        {
+            foreach (BoardCell item in currentBoardData)
+            {
+                item.Disable();
+            }
+        }
+        // Restart board settings to default
+        public void RestartBoard()
+        {
+            foreach (BoardCell item in currentBoardData)
+            {
+                item.Enable();
+                item.ChangeState(CellState.DEFAULT);
+            }
+        }
+
+    }
+}
+
